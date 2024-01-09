914e1de58866f14e085305b342b5b547f2d13779
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-09 18:55:41 -0500

    Lint and style peripheral tooling code

--- ci/parse_dotos.py
+++ ci/parse_dotos.py
@@ -1,11 +1,10 @@
-import configparser
 import json
 import sys
 
 res = {
-  'schemaVersion' : 1,
-  'label' : 'todos',
-  'message' : sys.argv[1],
-  'color' : 'orange',
+    "schemaVersion": 1,
+    "label": "todos",
+    "message": sys.argv[1],
+    "color": "orange",
 }
 print(json.dumps(res, sort_keys=True, indent=4))
