08fd9c357f872b89499bd7ffdb2f4f2bb9cec560
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-09 12:34:02 -0500

    Write docstrings

--- pylib/analyze_additive/_sample_knockout.py
+++ pylib/analyze_additive/_sample_knockout.py
@@ -2,6 +2,17 @@ import numpy as np
 
 
 def sample_knockout(dose: int, num_sites: int) -> np.array:
+    """Sample a knockout configuration with `dose` sites sampled uniformly over
+    `num_sites` genome positions.
+
+    Returns
+    -------
+    np.array
+        A binary mask where 1 represents a knockout at a site and 0 represents
+        no knockout.
+
+        The length of the array is equal to `num_sites`.
+    """
     res = np.zeros(num_sites)
     res[np.random.choice(num_sites, dose, replace=False)] = 1
     return res
