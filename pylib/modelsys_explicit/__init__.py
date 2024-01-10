e0b78f06d72a9b6f00b9e6028630e2c6684109ec
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-09 19:58:15 -0500

    Impl, test create_epistasis_matrix_disjoint

--- pylib/modelsys_explicit/__init__.py
+++ pylib/modelsys_explicit/__init__.py
@@ -1,10 +1,12 @@
 from ._CalcKnockoutEffectsAdditive import CalcKnockoutEffectsAdditive
 from ._create_additive_array import create_additive_array
+from ._create_epistasis_matrix_disjoint import create_epistasis_matrix_disjoint
 from ._GenomeExplicit import GenomeExplicit
 
 
 __all__ = [
     "CalcKnockoutEffectsAdditive",
     "create_additive_array",
+    "create_epistasis_matrix_disjoint",
     "GenomeExplicit",
 ]
