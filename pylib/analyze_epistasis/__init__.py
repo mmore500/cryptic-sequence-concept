88a77d6ea255039516b8e40a69ac7b6232ab772b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-11 18:55:48 -0500

    Impl, test naive epistasis assay

--- pylib/analyze_epistasis/__init__.py
+++ pylib/analyze_epistasis/__init__.py
@@ -1,8 +1,10 @@
+from ._assay_epistasis_naive import assay_epistasis_naive
 from ._describe_skeletons import describe_skeletons
 from ._jackknife_skeleton import jackknife_skeleton
 from ._skeletonize_naive import skeletonize_naive
 
 __all__ = [
+    "assay_epistasis_naive",
     "describe_skeletons",
     "jackknife_skeleton",
     "skeletonize_naive",
---
+++ pylib/test/test_analyze_epistasis/test_assay_epistasis_naive.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+
+from pylib.analyze_epistasis import (
+    assay_epistasis_naive,
+    describe_skeletons,
+    skeletonize_naive,
+)
+from pylib.modelsys_explicit import GenomeExplicit
+from pylib.modelsys_explicit import (
+    CalcKnockoutEffectsAdditive,
+    CalcKnockoutEffectsEpistasis,
+    create_additive_array,
+    create_epistasis_matrix_disjoint,
+    describe_additive_array,
+    describe_epistasis_matrix,
+    GenomeExplicit,
+)
+
+
+def test_assay_epistasis_naive_smoke():
+    num_sites = 1000
+    distn = lambda x: np.random.rand(x) * 0.7
+    additive_array = create_additive_array(num_sites, 0.04, distn)
+    epistasis_matrix = create_epistasis_matrix_disjoint(num_sites, 40, 4)
+    genome = GenomeExplicit(
+        [
+            CalcKnockoutEffectsAdditive(additive_array),
+            CalcKnockoutEffectsEpistasis(
+                epistasis_matrix, effect_size=(0.7, 1.6)
+            ),
+        ],
+    )
+
+    dfa = describe_additive_array(additive_array)
+    dfb = describe_epistasis_matrix(epistasis_matrix)
+    df_genome = pd.DataFrame.merge(dfa, dfb, on="site")
+    df_genome["site type"] = (
+        df_genome["additive site"].astype(int)
+        + df_genome["epistasis site"].astype(int) * 2
+    ).map(
+        {
+            0: "neutral",
+            1: "additive",
+            2: "epistasis",
+            3: "both",
+        }
+    )
+
+    num_skeletonizations = 5
+    skeletons = np.vstack(
+        [
+            skeletonize_naive(num_sites, genome.test_knockout)
+            for _ in range(num_skeletonizations)
+        ],
+    )
+
+    df_skeletons = describe_skeletons(skeletons, genome.test_knockout)
+
+    res = assay_epistasis_naive(df_skeletons)
+    assert isinstance(res, dict)
