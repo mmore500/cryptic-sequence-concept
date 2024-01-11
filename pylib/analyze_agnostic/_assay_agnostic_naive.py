54ac48fc52bace21f14ac0735e48fb8892126f85
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-11 18:17:27 -0500

    Write docstring for assay_agnostic_naive

--- pylib/analyze_agnostic/_assay_agnostic_naive.py
+++ pylib/analyze_agnostic/_assay_agnostic_naive.py
@@ -12,6 +12,43 @@ from ..auxlib._jackknife_mark_recapture_estimate import (
 def assay_agnostic_naive(
     skeletons_description_df: typing.Sequence[np.array],
 ) -> dict:
+    """Apply capture/release estimation over sites included within replicate
+    skeletonizations to estimate the prevalence of functional sites (i.e.,
+    epistatis and/or additive effects).
+
+    The mark-recapture estimation procedure is described to handle scenarios
+    involving variable capture probabilities (e.g., trap-shyness).
+
+    Parameters
+    ----------
+    skeletons_description_df : pandas.DataFrame
+        DataFrame describing replicate skeletonizations, produced by
+        `describe_skeletons`.
+
+        At least five skeletons should be provided for good-quality estimation.
+
+    Returns
+    -------
+    dict
+        A dictionary with the following keys:
+        - 'num sites estimate': The estimated total number of functional sites
+        within the genome, based on mark-recapture estimation over functional
+        sites detected by skeletonization.
+        - 'num sites 95% CI': A tuple containing lower and upper bound for a
+        95% confidence interval for the estimated number of functional sites.
+
+    Raises
+    ------
+    Warning
+        If fewer than five skeletons are provided, a warning is raised to inform
+        the user that the estimation may not be reliable.
+
+    See Also
+    --------
+    jackknife_mark_recapture_estimate
+        For details on the mark-recapture estimation procedure and references
+        to the literature describing the method.
+    """
     num_skeletons = ip.pophomogeneous(
         skeletons_description_df["skeleton outcome count, included"]
         + skeletons_description_df["skeleton outcome count, excluded"]
