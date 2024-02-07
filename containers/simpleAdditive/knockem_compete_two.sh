c59a277ea09b9df241a45bff74cd9c5089dcc556
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-07 01:57:21 -0500

    Add logging to simpleAdditive compete script

--- containers/simpleAdditive/knockem_compete_two.sh
+++ containers/simpleAdditive/knockem_compete_two.sh
@@ -3,14 +3,24 @@
 genome_alpha=$(python3 -m knockem.cli fetch-genome "$KNOCKEM_GENOME_ID_ALPHA")
 genome_beta=$(python3 -m knockem.cli fetch-genome "$KNOCKEM_GENOME_ID_BETA")
 
+echo "Genome alpha: $genome_alpha"
+echo "Genome beta: $genome_beta"
+
 # Run competition (example: summing genome values)
 sum_alpha=$(echo "$genome_alpha" | awk '{sum+=$1} END {print sum}')
 sum_beta=$(echo "$genome_beta" | awk '{sum+=$1} END {print sum}')
+
+echo "Sum alpha: $sum_alpha"
+echo "Sum beta: $sum_beta"
 
 # Logic to determine numalpha and numbeta would go here
 updatesElapsed=100
 numAlpha="$(awk "BEGIN {print 1000 * $sum_alpha / ($sum_alpha + $sum_beta)}")"
 numBeta="$(awk "BEGIN {print 1000 * $sum_beta / ($sum_alpha + $sum_beta)}")"
+
+echo "Updates elapsed: $updatesElapsed"
+echo "Num alpha: $numAlpha"
+echo "Num beta: $numBeta"
 
 sleep 5
 
