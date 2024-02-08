aaea1dd8d73da409122991e2a083641c98a27851
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-08 13:16:13 -0500

    Work around isort bug with #noqa

--- pylib/modelsys_explicit/__init__.py
+++ pylib/modelsys_explicit/__init__.py
@@ -2,7 +2,7 @@ from ._CalcKnockoutEffectsAdditive import CalcKnockoutEffectsAdditive
 from ._CalcKnockoutEffectsEpistasis import CalcKnockoutEffectsEpistasis
 from ._create_additive_array import create_additive_array
 from ._create_epistasis_matrix_disjoint import create_epistasis_matrix_disjoint
-from ._create_epistasis_matrix_overlapping import (
+from ._create_epistasis_matrix_overlapping import (  # noqa: isort
     create_epistasis_matrix_overlapping,
 )
 from ._describe_additive_array import describe_additive_array
