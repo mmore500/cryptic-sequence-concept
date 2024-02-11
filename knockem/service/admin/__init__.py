19b8c985017d16e813147494785cbb94a8c6d77e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-11 17:23:30 -0500

    Fix isort lint

--- knockem/service/admin/__init__.py
+++ knockem/service/admin/__init__.py
@@ -1,7 +1,6 @@
 from ._email_submission_status import email_submission_status
 from ._get_url_submission_get import get_url_submission_get
 
-
 __all__ = [
     "email_submission_status",
     "get_url_submission_get",
