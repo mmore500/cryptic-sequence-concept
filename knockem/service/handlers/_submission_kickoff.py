339244f336d7212e3b14e44e941f18ccbd9df2ba
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-10 19:42:45 -0500

    Bugfix: indent conditional within correct scope

--- knockem/service/handlers/_submission_kickoff.py
+++ knockem/service/handlers/_submission_kickoff.py
@@ -13,34 +13,34 @@ def submission_kickoff() -> int:
         orch.activate_submission(submissionId)
         num_launched += 1
 
-    if document["hasAssayNulldist"]:
-        assayId = rec.add_assay(
-            assayDesignation={},
-            assayType="nulldist",
-            competitionTimeoutSeconds=document["competitionTimeoutSeconds"],
-            containerEnv=document["containerEnv"],
-            containerImage=document["containerImage"],
-            dependsOnIds=[],
-            genomeIdAlpha=document["genomeIdAlpha"],
-            maxCompetitionsActive=document["maxCompetitionsActive"],
-            maxCompetitionRetries=document["maxCompetitionRetries"],
-            submissionId=submissionId,
-            userEmail=document["userEmail"],
-        )
-        orch.enqueue_assay(
-            assayDesignation={},
-            assayId=assayId,
-            assayType="nulldist",
-            competitionTimeoutSeconds=document["competitionTimeoutSeconds"],
-            containerEnv=document["containerEnv"],
-            containerImage=document["containerImage"],
-            dependsOnIds=[],
-            genomeIdAlpha=document["genomeIdAlpha"],
-            maxCompetitionsActive=document["maxCompetitionsActive"],
-            maxCompetitionRetries=document["maxCompetitionRetries"],
-            submissionId=submissionId,
-            userEmail=document["userEmail"],
-        )
+        if document["hasAssayNulldist"]:
+            assayId = rec.add_assay(
+                assayDesignation={},
+                assayType="nulldist",
+                competitionTimeoutSeconds=document["competitionTimeoutSeconds"],
+                containerEnv=document["containerEnv"],
+                containerImage=document["containerImage"],
+                dependsOnIds=[],
+                genomeIdAlpha=document["genomeIdAlpha"],
+                maxCompetitionsActive=document["maxCompetitionsActive"],
+                maxCompetitionRetries=document["maxCompetitionRetries"],
+                submissionId=submissionId,
+                userEmail=document["userEmail"],
+            )
+            orch.enqueue_assay(
+                assayDesignation={},
+                assayId=assayId,
+                assayType="nulldist",
+                competitionTimeoutSeconds=document["competitionTimeoutSeconds"],
+                containerEnv=document["containerEnv"],
+                containerImage=document["containerImage"],
+                dependsOnIds=[],
+                genomeIdAlpha=document["genomeIdAlpha"],
+                maxCompetitionsActive=document["maxCompetitionsActive"],
+                maxCompetitionRetries=document["maxCompetitionRetries"],
+                submissionId=submissionId,
+                userEmail=document["userEmail"],
+            )
 
     if num_launched > 0:
         logging.info(f"Launched {num_launched} submissions.")
